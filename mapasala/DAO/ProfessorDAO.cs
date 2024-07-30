b824375403a9f9e3f26b4175280b6654d62649f8
nicoli <santosbuenonicoligabriely@gmail>
2024-07-30 10:52:14 -0300

    DAO

---
+++ mapasala/DAO/ProfessorDAO.cs
@@ -0,0 +1,25 @@
+﻿using Model.Entidades;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mapasala.DAO
+{
+    public class ProfessorDAO
+    {
+        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsasql;";
+        private SqlConnection Conexao;
+        public ProfessorDAO()
+        {
+            Conexao = new SqlConnection(LinhaConexao);
+        }
+        public void Inserir(ProfessoresEntidade professor)
+        {
+            Conexao.Open();
+            string query = "Insert into Professores "
+        }
+       
+        
+    }
+}
